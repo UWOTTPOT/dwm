3bab7623588252564acfe65ed73b195e54db0339
Jordan Hammond <jordanhammond17@gmail.com>
2024-03-20 12:33:49 +0000

    update config

--- dmenu/config.h
+++ dmenu/config.h
@@ -1,23 +1,22 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;  /* -F  option; if 0, dmenu doesn't use fuzzy matching  */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"JetBrainsMono Nerd Font:size=12"
-};
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *fonts[] = {"JetBrainsMono Nerd Font:size=12"};
+static const char *prompt =
+    NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#22ba91" },
-	[SchemeSelHighlight] = { "#ffc978", "#22ba91" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+    /*     fg         bg       */
+    [SchemeNorm] = {"#bbbbbb", "#222222"},
+    [SchemeSel] = {"#eeeeee", "#b88b11"},
+    [SchemeSelHighlight] = {"#ffc978", "#b88b11"},
+    [SchemeNormHighlight] = {"#ffc978", "#222222"},
+    [SchemeOut] = {"#000000", "#00ffff"},
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines = 0;
 
 /*
  * Characters not considered part of a word while deleting words
